6a5e2b7a404c2f017cb7cd8ee2bd8d9d291c0305
Ardhan <153461371+ArdhanAzhraAzmi@users.noreply.github.com>
2024-12-03 21:52:42 +0700

    Add files via upload

---
+++ Pertemuan4_50421187/src/main/java/me/ardhan/pertemuan4_50421187/Pertemuan4_50421187.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package me.ardhan.pertemuan4_50421187;
+
+/**
+ *
+ * @author Ardhan
+ */
+public class Pertemuan4_50421187 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
